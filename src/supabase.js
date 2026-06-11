773f6fe2d8d562e464398945eb17611f8cd40b26
MINGW64 <sethi@Mordrex>
2026-06-11 20:30:00 +0100

    hardcode supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://fqqbglkqmhbajmolxhei.supabase.co'
-const SUPABASE_ANON_KEY = 'your_full_anon_key_here'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxcWJnbGtxbWhiYWptb2x4aGVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNjMwNzMsImV4cCI6MjA2MzkzOTA3M30.zPuhfkf7J5BemjbZZx7nrWbbUAGriYMM'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
