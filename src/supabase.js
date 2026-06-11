b926a0e9119733fce07b5ddefcab796c7dd65ac3
MINGW64 <sethi@Mordrex>
2026-06-11 21:52:39 +0100

    update to new supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://fqqbglkqmhbajmolxhei.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_BqfNPuu_Fno9VRbmC33uOw_Vwtw6KA-'
+const SUPABASE_ANON_KEY = 'sb_publishable_Xq7Jp9MMkTGViY5MHu8Dxw_6mJtg6CT'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
